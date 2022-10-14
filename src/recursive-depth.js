d9beb922407d436bd30d44d8939d8069bae1d229
EXisTAnZ <ozim_ssspam@mail.ru>
2022-10-14 21:54:29 +0300

    feat: started implement of recursive depth 	modified:   src/recursive-depth.js

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -13,9 +13,13 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    calculateDepth(arr, q = 0) {
+      let res = q;
+      if (Array.isArray(arr)) {
+        arr.forEach(el => this.calculateDepth(el))
+        res++;
+      };
+      return res;
   }
 }
 
