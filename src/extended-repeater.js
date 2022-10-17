2dea16748b254a96bb56aa9c11f817b75e049e2e
EXisTAnZ <ozim_ssspam@mail.ru>
2022-10-18 00:21:51 +0300

    feat: repeater implements 	modified:   src/extended-repeater.js

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,17 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  let defOptions = {
+    repeatTimes: options.repeatTimes || 1,
+    separator: options.separator || '+',
+    addition: options.addition || '',
+    additionRepeatTimes: options.additionRepeatTimes || 1,
+    additionSeparator: options.additionSeparator || ''
+  }
+  let strAddition = (defOptions.addition+defOptions.additionSeparator).repeat(defOptions.additionRepeatTimes).slice(0,-defOptions.additionSeparator.length);
+  console.log(strAddition);
+  return (str + strAddition + defOptions.separator).repeat(defOptions.repeatTimes).slice(0,-defOptions.separator.length);
 }
 
 module.exports = {
